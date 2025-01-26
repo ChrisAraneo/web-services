b5be7a50bb9a73e8f2380229f2105338752246d0
Krzysztof Pająk <chris.araneo@gmail.com>
2025-01-26 16:19:35 +0100

    feat: download repo zips and then unzip them

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,48 @@
-export function concat(str1: string, str2: string): string {
-  return str1 + str2;
+import path from 'node:path';
+
+import { download } from './download';
+import { unzip } from './unzip';
+
+const REPOSITORIES = ['minecraft-players-number-notifier', 'email-service'];
+
+function downloadRepository(repository: string) {
+  return new Promise<string>((resolve, reject) => {
+    console.log(`Downloading ${repository}`);
+
+    download(
+      `https://codeload.github.com/ChrisAraneo/${repository}/zip/refs/heads/master`,
+      path.normalize(process.cwd() + `/${repository}.zip`),
+    )
+      .then(() => {
+        resolve(repository);
+      })
+      .catch((error: unknown) => {
+        reject(error);
+      });
+  });
 }
+
+REPOSITORIES.forEach((repository) => {
+  downloadRepository(repository)
+    .then(() => {
+      console.log('Downloaded repository ' + repository);
+
+      const archive = path.normalize(`${process.cwd()}/${repository}.zip`);
+      const output = path.normalize(`${process.cwd()}/${repository}`);
+
+      console.log(`Extracting file ${archive}`);
+
+      unzip(archive, output)
+        .then(() => {
+          console.log(`Extracted ${archive} into ${output}`);
+        })
+        .catch((error: unknown) => {
+          console.error(`Extracting error (${archive})`);
+          console.error(error);
+        });
+    })
+    .catch((error) => {
+      console.error('Error during downloading repository');
+      console.error(error);
+    });
+});
