a3562714884536ac7c96779aa3d34dd10d0e0fe7
Krzysztof Pająk <chris.araneo@gmail.com>
2025-03-02 20:30:42 +0100

    feat: run email service and health check service

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('WIP');
+import { EmailService } from '@chris.araneo/email-service';
+import { HealthCheckService } from '@chris.araneo/health-check';
+import { Logger } from '@chris.araneo/logger';
+
+(() => {
+  const logger = new Logger('debug');
+
+  new EmailService('/email', 5050, logger).listen();
+
+  new HealthCheckService('/health', 9339, logger).listen();
+})();
